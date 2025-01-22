b890a52b13df49106e9d7793a9e8226192d45afe
deblur99 <deblur99@knowledgepoint.co.kr>
2025-01-22 19:37:08 +0900

    Initial Commit

---
+++ BootScreenLikeScreenSaver/BootScreenLikeScreenSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootScreenLikeScreenSaverApp.swift
+//  BootScreenLikeScreenSaver
+//
+//  Created by 한현민 on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BootScreenLikeScreenSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BootScreenLikeScreenSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BootScreenLikeScreenSaver
+//
+//  Created by 한현민 on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
